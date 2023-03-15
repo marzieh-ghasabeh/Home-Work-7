20f0cd2dcdaac7b7fc710169ac8e391afda98e63
Marzieh Ghasabeh <marzieh.ghasabeh@gmail.com>
2023-03-15 14:08:08 +0330

    Create two elements of MetalElement and NonMetalElement

--- src/Q2/ElementArray.java
+++ src/Q2/ElementArray.java
@@ -1,5 +1,14 @@
 package Q2;
 
 public class ElementArray {
+    static Element[] elementArray = {new MetalElement("c", 2, 2.1),
+            new MetalElement("c", 2, 2.1),
+            new NonMetalElement("c", 2, 2.1),
+            new NonMetalElement("c", 2, 2.1),};
 
+    public static void main(String[] args) {
+        for (Element element : elementArray) {
+            System.out.println(element);
+        }
+    }
 }
