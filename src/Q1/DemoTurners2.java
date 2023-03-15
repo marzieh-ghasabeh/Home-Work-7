628530ff5354c4cd055773d7cf1d6c80a2aee25f
Marzieh Ghasabeh <marzieh.ghasabeh@gmail.com>
2023-03-16 01:23:33 +0330

    DemoTurners application renamed DemoTurners2 application

--- src/Q1/DemoTurners2.java
+++ src/Q1/DemoTurners2.java
@@ -1,6 +1,6 @@
 package Q1;
 
-public class DemoTurners {
+public class DemoTurners2 {
     public static void main(String[] args) {
         Turner leaf = new Leaf();
         Turner page = new Page();
