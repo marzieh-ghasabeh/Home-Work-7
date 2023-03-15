293ec018db96d166a1d90eceee02fd30b27048d8
Marzieh Ghasabeh <marzieh.ghasabeh@gmail.com>
2023-03-15 12:38:46 +0330

    create one extended class named NonMetalElement

---
+++ src/Q2/NonMetalElement.java
@@ -0,0 +1,11 @@
+package Q2;
+
+public class NonMetalElement extends Element{
+    public NonMetalElement(String symbol, int atomic_number, double atomic_weight) {
+        super(symbol, atomic_number, atomic_weight);
+    }
+
+    public void describeElement(){
+
+    }
+}
