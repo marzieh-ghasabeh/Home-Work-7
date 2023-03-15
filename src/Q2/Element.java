d7091cb889c727186faa9b8458e23d49ef53c628
Marzieh Ghasabeh <marzieh.ghasabeh@gmail.com>
2023-03-15 12:18:59 +0330

    Create an abstract class named Element

---
+++ src/Q2/Element.java
@@ -0,0 +1,26 @@
+package Q2;
+
+public abstract class Element {
+    private String symbol;
+    private int atomic_number;
+    private double atomic_weight;
+
+    public Element(String symbol, int atomic_number, double atomic_weight) {
+        this.symbol = symbol;
+        this.atomic_number = atomic_number;
+        this.atomic_weight = atomic_weight;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAtomic_number() {
+        return atomic_number;
+    }
+
+    public double getAtomic_weight() {
+        return atomic_weight;
+    }
+}
+
